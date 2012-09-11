f7e44c76002949dc042bcd87f48f511a7c0b7d1a
Kyle Machulis <kyle@nonpolynomial.com>
2012-09-11 16:29:57 -0700

    whitespace and comment cleanup

--- dom/bluetooth/BluetoothHfpManager.cpp
+++ dom/bluetooth/BluetoothHfpManager.cpp
@@ -38,151 +38,12 @@ BluetoothHfpManager::GetManager()
   return sInstance;
 }
 
-/*
-bool
-BluetoothHfpManager::ReachedMaxConnection()
-{
-  // Now we can only have one conenction at a time
-  return mConnected;
-}
-
-void
-BluetoothHfpManager::Disconnect()
-{
-  void* ret;
-
-  LOG("Disconnect");
-
-  if (mSocket != NULL)
-  {
-    sStopEventLoopFlag = true;
-
-    LOG("Before join");
-    pthread_join(mEventThread, &ret);
-    LOG("After join");
-
-    mSocket->Disconnect();
-
-    usleep(2000);
-
-    BluetoothScoManager* scoManager = BluetoothScoManager::GetManager();
-    scoManager->Disconnect();
-
-    delete mSocket;
-    mSocket = NULL;
-  }
-
-  mConnected = false;
-}
-
-
-void
-BluetoothHfpManager::Close()
-{
-  void* ret;
-
-  if (mServerSocket != NULL) {
-    mServerSocket->Disconnect();
-
-    sStopAcceptFlag = true;
-    pthread_join(mAcceptThread, &ret);
-
-    delete mServerSocket;
-    mServerSocket = NULL;
-  }
-}
-
-bool
-BluetoothHfpManager::Listen(int channel)
-{
-  if (channel <= 0)
-    return false;
-
-  if (mServerSocket != NULL)
-    return false;
-
-  while(true) {
-    if (mServerSocket == NULL || !mServerSocket->Available()) {
-      mServerSocket = new BluetoothSocket(BluetoothSocket::TYPE_RFCOMM);
-      LOG("Create a new BluetoothServerSocket for listening");
-    }
-
-    LOG("Listening to channel %d", channel);
-
-    mChannel = channel;
-
-    usleep(500);
-    int errno = mServerSocket->Listen(channel);
-
-    // 98 :EADDRINUSE
-    if (errno == 0) {
-      LOG("Channel is OK.");
-      break;
-    } else if (errno == 98) {
-      LOG("Channel is still in use.");
-      mServerSocket->Close();
-    } else {
-      LOG("Unexpected error: %d", errno);
-      mServerSocket->Close();
-    }
-  }
-
-  pthread_create(&(mAcceptThread), NULL, BluetoothHfpManager::AcceptInternal, mServerSocket);
-
-  return true;
-}
-
-void*
-BluetoothHfpManager::AcceptInternal(void* ptr)
-{
-  BluetoothSocket* serverSocket = static_cast<BluetoothSocket*>(ptr);
-
-  // TODO(Eric)
-  // Need to let it break the while loop
-  sStopAcceptFlag = false;
-
-  while (!sStopAcceptFlag) {
-    int newFd = serverSocket->Accept();
-
-    if (newFd <= 0) {
-      LOG("Error occurs after accepting:%s", __FUNCTION__);
-    }
-
-    BluetoothHfpManager* manager = BluetoothHfpManager::GetManager();
-
-    if (manager->mSocket != NULL) {
-      manager->mSocket->Disconnect();
-    }
-
-    manager->mSocket = new BluetoothSocket(BluetoothSocket::TYPE_RFCOMM, newFd);
-
-    pthread_create(&manager->mEventThread, NULL, BluetoothHfpManager::MessageHandler, manager->mSocket);
-
-    usleep(5000);
-
-    // Connect ok, next : establish a SCO link
-    BluetoothScoManager* scoManager = BluetoothScoManager::GetManager();
-
-    if (!scoManager->IsConnected()) {
-      const char* address = serverSocket->GetAddress();
-      LOG("[ERIC] SCO address : %s", address);
-      scoManager->Connect(address);
-    }
-
-    // reply_vgs(newFd);
-  }
-
-  return NULL;
-}
-
-*/
-
 BluetoothHfpManager::~BluetoothHfpManager()
 {
 
 }
 
-void 
+void
 BluetoothHfpManager::ReplyCindCurrentStatus()
 {
   const char* str = "+CIND: 5,5,1,0,0,0,0";
@@ -191,31 +52,27 @@ BluetoothHfpManager::ReplyCindCurrentStatus()
   strcat(response, kHfpCRLF);
   strcat(response, str);
   strcat(response, kHfpCRLF);
-    
+
   mozilla::ipc::SocketRawData* s = new mozilla::ipc::SocketRawData(response);
   SendSocketData(s);
 }
 
-void 
+void
 BluetoothHfpManager::ReplyCindRange()
 {
   const char* str = "+CIND: (\"battchg\",(0-5)),(\"signal\",(0-5)),(\"service\",(0,1)),(\"call\",(0,1)),(\"callsetup\",(0-3)),(\"callheld\",(0-2)),(\"roam\",(0,1))";
-/*
-	+CIND: (\"service\",(0-1)),(\"call\",(0-1)),(\"callsetup\",(0-3)),		\
-                     (\"callheld\",(0-2)),(\"signal\",(0-5)),(\"roam\",(0-1)), \
-                     (\"battchg\",(0-5))";  */
-                     
+
 	char response[256] = {'\0'};
 
   strcat(response, kHfpCRLF);
   strcat(response, str);
   strcat(response, kHfpCRLF);
-    
+
   mozilla::ipc::SocketRawData* s = new mozilla::ipc::SocketRawData(response);
   SendSocketData(s);
 }
 
-void 
+void
 BluetoothHfpManager::ReplyCmer(bool enableIndicator)
 {
   const char* str = enableIndicator ? "+CMER: 3,0,0,1" : "+CMER: 3,0,0,0";
@@ -224,12 +81,12 @@ BluetoothHfpManager::ReplyCmer(bool enableIndicator)
   strcat(response, kHfpCRLF);
   strcat(response, str);
   strcat(response, kHfpCRLF);
-    
+
   mozilla::ipc::SocketRawData* s = new mozilla::ipc::SocketRawData(response);
   SendSocketData(s);
 }
 
-void 
+void
 BluetoothHfpManager::ReplyChldRange()
 {
 	char response[256] = {'\0'};
@@ -237,33 +94,33 @@ BluetoothHfpManager::ReplyChldRange()
   strcat(response, kHfpCRLF);
   strcat(response, "+CHLD: (0,1,2,3)");
   strcat(response, kHfpCRLF);
-    
+
   mozilla::ipc::SocketRawData* s = new mozilla::ipc::SocketRawData(response);
   SendSocketData(s);
 }
 
-void 
+void
 BluetoothHfpManager::ReplyBrsf()
-{    
+{
 	char response[256] = {'\0'};
 
   strcat(response, kHfpCRLF);
   strcat(response, "+BRSF: 352");
   strcat(response, kHfpCRLF);
-    
+
   mozilla::ipc::SocketRawData* s = new mozilla::ipc::SocketRawData(response);
   SendSocketData(s);
 }
 
-void 
+void
 BluetoothHfpManager::ReplyOk()
-{    
+{
   char response[256] = {'\0'};
 
   strcat(response, kHfpCRLF);
   strcat(response, "OK");
-  strcat(response, kHfpCRLF);  
-    
+  strcat(response, kHfpCRLF);
+
   mozilla::ipc::SocketRawData* s = new mozilla::ipc::SocketRawData(response);
   SendSocketData(s);
 }
