ba212e04f114720839aa6991cba6ac5266373988
Eric Chou <echou@mozilla.com>
2013-03-05 12:20:25 +0800

    Bug 841576 - Check if nsTHashtable has been initialized before calling Init()

--- dom/bluetooth/linux/BluetoothDBusService.cpp
+++ dom/bluetooth/linux/BluetoothDBusService.cpp
@@ -1633,8 +1633,13 @@ BluetoothDBusService::StartInternal()
     return NS_ERROR_FAILURE;
   }
 
-  sPairingReqTable.Init();
-  sAuthorizeReqTable.Init();
+  if (!sPairingReqTable.IsInitialized()) {
+    sPairingReqTable.Init();
+  }
+
+  if (!sAuthorizeReqTable.IsInitialized()) {
+    sAuthorizeReqTable.Init();
+  }
 
   BluetoothValue v;
   nsString replyError;
