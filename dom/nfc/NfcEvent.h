8dba2bd46117d68fa09d96b062a112db6d7cf294
Garner Lee <garner.lee@telekom.com>
2013-08-29 18:06:29 -0700

    Config request message impl in gecko.

--- dom/nfc/NfcEvent.h
+++ dom/nfc/NfcEvent.h
@@ -24,7 +24,7 @@ public:
   NS_DECL_ISUPPORTS_INHERITED
   NS_FORWARD_TO_NSDOMEVENT
   NS_DECL_NSIDOMMOZNFCEVENT
-  NS_DECL_CYCLE_COLLECTION_CLASS_INHERITED(NfcEvent, nsDOMEvent)
+  //NS_DECL_CYCLE_COLLECTION_CLASS_INHERITED(NfcEvent, nsDOMEvent)
 
   static already_AddRefed<NfcEvent>
   Create(EventTarget* aOwner, const nsAString& aMessage);
