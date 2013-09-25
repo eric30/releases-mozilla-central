b69d099904d38937554ebb11d29bdd2a53641c81
Garner Lee <garner.lee@telekom.com>
2013-09-25 14:37:25 -0700

    Contributers and remove rules.mk line.

--- dom/nfc/MozNdefRecord.cpp
+++ dom/nfc/MozNdefRecord.cpp
@@ -4,6 +4,8 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+/* Copyright © 2013 Deutsche Telekom, Inc. */
+
 #include "MozNdefRecord.h"
 #include "mozilla/dom/MozNdefRecordBinding.h"
 #include "nsContentUtils.h"
